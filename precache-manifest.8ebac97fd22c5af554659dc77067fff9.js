b86ce4ada112682577a14b8a222fe2b9067a1598
j2teamlbh <j2teamlbh@gmail.com>
2019-05-23 21:58:26 +0700

    Updates

---
+++ precache-manifest.8ebac97fd22c5af554659dc77067fff9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af762ecf4bdab21eab6455af47e76628",
+    "url": "/J2TeamLive/index.html"
+  },
+  {
+    "revision": "5d8cfd45102a805cf88c",
+    "url": "/J2TeamLive/static/css/main.5798f1f4.chunk.css"
+  },
+  {
+    "revision": "d0263a828327f5e07d3f",
+    "url": "/J2TeamLive/static/js/2.df406e02.chunk.js"
+  },
+  {
+    "revision": "5d8cfd45102a805cf88c",
+    "url": "/J2TeamLive/static/js/main.d26d44c8.chunk.js"
+  },
+  {
+    "revision": "b26e50b35e21402f88c8",
+    "url": "/J2TeamLive/static/js/runtime~main.010ebe5b.js"
+  },
+  {
+    "revision": "16f6c1029632626f154cd976954f7e70",
+    "url": "/J2TeamLive/static/media/ANGRY.16f6c102.gif"
+  },
+  {
+    "revision": "7c8dac3b117e8d253e5ade1e9c60b45f",
+    "url": "/J2TeamLive/static/media/HAHA.7c8dac3b.gif"
+  },
+  {
+    "revision": "7b5930788544fc35607872cd70903bb0",
+    "url": "/J2TeamLive/static/media/LIKE.7b593078.gif"
+  },
+  {
+    "revision": "801526e1d93b2c3925a8a2bba8ac0a1e",
+    "url": "/J2TeamLive/static/media/LOVE.801526e1.gif"
+  },
+  {
+    "revision": "447909adfdee768519f7315d9b1cc904",
+    "url": "/J2TeamLive/static/media/SAD.447909ad.gif"
+  },
+  {
+    "revision": "9baf7e7f86cd5862fb3496c2c11ecd22",
+    "url": "/J2TeamLive/static/media/WOW.9baf7e7f.gif"
+  },
+  {
+    "revision": "904748078e6d2ecddb54443bc8f512a1",
+    "url": "/J2TeamLive/static/media/logo.90474807.png"
+  }
+]);
